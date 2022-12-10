da31f4934851efdc4a6e208a60b96b0a77e304eb
alphatuneaudio <alphatuneaudio@gmail.com>
2022-12-11 02:17:19 +0700

    99

--- pages/index.js
+++ pages/index.js
@@ -235,6 +235,10 @@ const Home = () => {
 
   <br/>
 
+                                <div style={{ width:"99%" }} name="media" id="media">
+                                    <iframe width="444" height="444" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/3525754&auto_play=false&hide_related=false&show_comments=true&show_user=false&show_reposts=false&visual=false"></iframe>
+
+                                </div>
 
                                 <div className="divider"></div>
                                 <div style={{ width: "455%" }}>
