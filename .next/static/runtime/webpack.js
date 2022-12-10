c002fbc7d17bc8edd809e052296b59b769905971
alphatuneaudio <alphatuneaudio@gmail.com>
2022-12-10 19:46:01 +0700

    222

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "01d31e9e4984eacc3bfc";
+/******/ 	var hotCurrentHash = "ff104b55a318ad2e67dc";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -237,7 +237,7 @@ const Home = () => {
 <br/>
 
                                 <div style={{ width: "80%" }} name="media" id="media" >
-                                    <iframe width="333" height="444" scrolling="yes" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/3525754&auto_play=false&hide_related=false&show_comments=true&show_user=false&show_reposts=false&visual=false"></iframe>
+                                    <iframe width="222" height="444" scrolling="yes" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/3525754&auto_play=false&hide_related=false&show_comments=true&show_user=false&show_reposts=false&visual=false"></iframe>
 
                                 </div>
 
@@ -246,7 +246,7 @@ const Home = () => {
                                     <TwitterTimelineEmbed
                                         sourceType="profile"
                                         screenName="alexplaysmusic"
-                                        options={{ height: 860, width: "265" }}
+                                        options={{ height: 860, width: "222" }}
                                     />
                                 </div>
 
