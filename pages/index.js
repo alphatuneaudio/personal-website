ebf09530ba30068e75ee57e061d250cdc27a2285
alphatuneaudio <alphatuneaudio@gmail.com>
2022-12-10 19:02:53 +0700

    new

--- pages/index.js
+++ pages/index.js
@@ -234,8 +234,8 @@ const Home = () => {
                             <section className="contaier">
 <br/>
 
-                                <div style={{ width: "333" }} name="media" id="media" >
-                                    <iframe width="333" height="333" scrolling="yes" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/3525754&auto_play=false&hide_related=false&show_comments=true&show_user=false&show_reposts=false&visual=false"></iframe>
+                                <div style={{ width: "188" }} name="media" id="media" >
+                                    <iframe width="444" height="333" scrolling="yes" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/3525754&auto_play=false&hide_related=false&show_comments=true&show_user=false&show_reposts=false&visual=false"></iframe>
 
                                 </div>
 
