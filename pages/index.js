c225be3fbe55489fb443b714447945bb8f6685d1
alphatuneaudio <alphatuneaudio@gmail.com>
2022-12-11 01:54:36 +0700

    de

--- pages/index.js
+++ pages/index.js
@@ -147,7 +147,7 @@ const Home = () => {
                                 <img
                                     alt="image"
                                     src="/playground_assets/bottom.svg"
-                                    className="bottom-wave-image"tyle={{ marginLeft: -33}}>
+                                    className="bottom-wave-image"style={{ marginLeft: -222}}>
                                 />
                                 <img
                                     alt="image"
